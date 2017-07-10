5ba058c4a523e622e1ad7ccf696a62b05886d4e1
Stefano Pigozzi <ste.pigozzi@gmail.com>
2017-07-10 18:42:55 +0200

    jsifmisdjfivgj

---
+++ roles/priority.py
@@ -0,0 +1,4 @@
+from .roles import *
+
+# Ordine in cui vengono eseguiti i onendday dei vari ruoli.
+rolepriority = [Mifioso, Investigatore, Corrotto, Disastro, Angelo, Derek, Stagista, Terrorista, Mamma, SignoreDelCaos, Servitore]
